191943651b4c1fb3211218a499ea85129af33895
Karthi Anbazhagan <karthithiruannamalai@gmail.com>
2026-04-20 19:02:27 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
+import os
+import logging
 from fastapi import FastAPI
+
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
+SERVICE_NAME = os.getenv("SERVICE_NAME", "unknown")
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello Karthi, from FastAPI!!!"}
+    logging.info(f"Request hit {SERVICE_NAME}")
+    return {"message": f"Hello from {SERVICE_NAME}"}
