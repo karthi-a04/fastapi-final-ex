315676953b281d46e95a5445d3368c15f4d2492b
Karthi Anbazhagan <karthithiruannamalai@gmail.com>
2026-04-20 20:21:49 +0530

    Update code

--- app/main.py
+++ app/main.py
@@ -1,14 +1,21 @@
 import os
 import logging
 from fastapi import FastAPI
+from pythonjsonlogger import jsonlogger
 
-logging.basicConfig(level=logging.INFO)
+# Set up JSON logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = jsonlogger.JsonFormatter()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 app = FastAPI()
 
-SERVICE_NAME = os.getenv("SERVICE_NAME", "unknown")
+SERVICE_NAME = os.getenv("SERVICE_NAME", "unknown_service")
 
 @app.get("/")
 def read_root():
-    logging.info(f"Request hit {SERVICE_NAME}")
+    logging.info("Request hit", extra={"service": SERVICE_NAME})
     return {"message": f"Hello from {SERVICE_NAME}"}
